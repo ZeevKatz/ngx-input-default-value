d3146c4f4a55b9da83a7bd2c8c98937b3023233f
ZeevKatz <zeevk6@gmail.com>
2019-05-05 11:27:12 +0300

    test: add tests for input default value decorator

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,154 +1,145 @@
-import { Subject } from 'rxjs';
-import {  } from '../src';
+import { InputDefaultValue } from "../src";
 
-function createObserver() {
-  return {
-    next: jest.fn(),
-    error: jest.fn(),
-    complete: jest.fn(),
-  };
-}
-
-describe('untilDestroyed', () => {
-  it('should not destroy other instances', () => {
-    const spy = createObserver();
-    const spy2 = createObserver();
-
+describe("InputDefaultValue", () => {
+  it("should assign the default value when value is null or undefined", () => {
     class Test {
-      obs;
-
-      ngOnDestroy() {}
-
-      subscribe( spy ) {
-        this.obs = new Subject().pipe(untilDestroyed(this)).subscribe(spy);
-      }
+      @InputDefaultValue("Default string")
+      property: string;
     }
-
-    const component1 = new Test();
-    const component2 = new Test();
-    component1.subscribe(spy);
-    component2.subscribe(spy2);
-    component1.ngOnDestroy();
-    expect(spy.complete).toHaveBeenCalledTimes(1);
-    expect(spy2.complete).not.toHaveBeenCalled();
-    component2.ngOnDestroy();
-    expect(spy2.complete).toHaveBeenCalledTimes(1);
-  });
-
-  it('should work with multiple observables', () => {
-    const spy = createObserver();
-    const spy2 = createObserver();
-    const spy3 = createObserver();
-
-    class Test {
-      obs = new Subject().pipe(untilDestroyed(this)).subscribe(spy);
-      obs2 = new Subject().pipe(untilDestroyed(this)).subscribe(spy2);
-      obs3 = new Subject().pipe(untilDestroyed(this)).subscribe(spy3);
-
-      ngOnDestroy() {}
-    }
-
     const instance = new Test();
-    instance.ngOnDestroy();
-    expect(spy.complete).toHaveBeenCalledTimes(1);
-    expect(spy2.complete).toHaveBeenCalledTimes(1);
-    expect(spy3.complete).toHaveBeenCalledTimes(1);
+    instance.property = null;
+    expect(instance.property).toEqual("Default string");
+    instance.property = undefined;
+    expect(instance.property).toEqual("Default string");
   });
-
-  it('should work with classes that are not components', () => {
-    const spy = createObserver();
-
-    class Test {
-      obs = new Subject().pipe(untilDestroyed(this, 'destroy')).subscribe(spy);
-
-      destroy() {
-        console.log('called');
+  describe("should work with primitives", () => {
+    it("should work with string", () => {
+      class Test {
+        @InputDefaultValue("Default string")
+        property: string;
       }
-    }
-
-    const instance = new Test();
-    instance.destroy();
-    expect(spy.complete).toHaveBeenCalledTimes(1);
+      const instance = new Test();
+      instance.property = "New string";
+      expect(instance.property).toEqual("New string");
+    });
+    it("should work with boolean", () => {
+      class Test {
+        @InputDefaultValue(true)
+        property: boolean;
+      }
+      const instance = new Test();
+      instance.property = false;
+      expect(instance.property).toEqual(false);
+    });
+    it("should work with number", () => {
+      class Test {
+        @InputDefaultValue(0)
+        property: number;
+      }
+      const instance = new Test();
+      instance.property = 1;
+      expect(instance.property).toEqual(1);
+    });
   });
-});
-
-describe('it should work anywhere', () => {
-  const spy = createObserver();
-  const spy2 = createObserver();
-  const spy3 = createObserver();
-
-  class LoginComponent {
-    dummy = new Subject().pipe(untilDestroyed(this)).subscribe(spy);
-
-    constructor() {
-      new Subject().pipe(untilDestroyed(this)).subscribe(spy2);
-    }
-
-    ngOnInit() {
-      new Subject().pipe(untilDestroyed(this)).subscribe(spy3);
-    }
-
-    ngOnDestroy() {}
-  }
-
-  it('should unsubscribe', () => {
-    const instance = new LoginComponent();
-    instance.ngOnInit();
-    instance.ngOnDestroy();
-    expect(spy.complete).toHaveBeenCalledTimes(1);
-    expect(spy2.complete).toHaveBeenCalledTimes(1);
-    expect(spy3.complete).toHaveBeenCalledTimes(1);
+  describe("should work with objects", () => {
+    it("should work with simple object", () => {
+      interface SimpleObject {
+        string: string;
+        number: number;
+        boolean: boolean;
+      }
+      class Test {
+        @InputDefaultValue<SimpleObject>({
+          string: "Default string",
+          number: 0,
+          boolean: true
+        })
+        property: Partial<SimpleObject>;
+      }
+      const instance = new Test();
+      instance.property = { string: "New string" };
+      expect(instance.property).toEqual({
+        string: "New string",
+        number: 0,
+        boolean: true
+      });
+    });
+    it("should work with nested object", () => {
+      type DeepPartial<T> = {
+        [P in keyof T]?: T[P] extends Array<infer U>
+          ? Array<DeepPartial<U>>
+          : DeepPartial<T[P]>
+      };
+      interface NestedObject {
+        string: string;
+        simpleObject: {
+          string: string;
+          number: number;
+          boolean: boolean;
+        };
+      }
+      class Test {
+        @InputDefaultValue<NestedObject>({
+          string: "Default string",
+          simpleObject: {
+            string: "Default string",
+            number: 0,
+            boolean: true
+          }
+        })
+        property: DeepPartial<NestedObject>;
+      }
+      const instance = new Test();
+      instance.property = { simpleObject: { string: "New string" } };
+      expect(instance.property).toEqual({
+        string: "Default string",
+        simpleObject: {
+          string: "New string",
+          number: 0,
+          boolean: true
+        }
+      });
+    });
   });
-});
-
-describe('it should throw', () => {
-  const spy = createObserver();
-
-  class LoginComponent {
-    dummy = new Subject().pipe(untilDestroyed(this)).subscribe(spy);
-  }
-
-  it('should throw when destroy method doesnt exist', () => {
-    expect(function() {
-      new LoginComponent();
-    }).toThrow(
-      `LoginComponent is using untilDestroyed but doesn't implement ngOnDestroy`
-    );
+  describe("should work with arrays", () => {
+    it("should work with simple array", () => {
+      class Test {
+        @InputDefaultValue<[string, number, boolean]>([
+          "Default string",
+          0,
+          true
+        ])
+        property: Partial<[string, number, boolean]>;
+      }
+      const instance = new Test();
+      instance.property = ["New string"];
+      expect(instance.property).toEqual(["New string", 0, true]);
+    });
+    it("should work with multidimensional array", () => {
+      class Test {
+        @InputDefaultValue<number[][]>([[0, 1, 2], [3, 4, 5]])
+        property: number[][];
+      }
+      const instance = new Test();
+      instance.property = [[-1]];
+      expect(instance.property).toEqual([[-1, 1, 2], [3, 4, 5]]);
+    });
   });
-
-  it('should work with super', () => {
-    class A {
-      ngOnDestroy() {}
-    }
-
-    class B extends A {
-      dummy = new Subject().pipe(untilDestroyed(this)).subscribe(spy);
-    }
-
-    expect(function() {
-      new B();
-    }).not.toThrow(
-      `B is using untilDestroyed but doesn't implement ngOnDestroy`
-    );
-  });
-});
-
-describe('inheritance', () => {
-  it('should work with subclass', () => {
-    const spy = createObserver();
-
-    class Parent {
-
-      ngOnDestroy() {}
-    }
-
-    class Child extends Parent {
-      constructor() { super();}
-      obs = new Subject().pipe(untilDestroyed(this)).subscribe(spy);
-    }
-
-    const instance = new Child();
-    instance.ngOnDestroy();
-    expect(spy.complete).toHaveBeenCalledTimes(1);
+  describe("inheritance", () => {
+    it("should work with subclass", () => {
+      class Parent {
+        @InputDefaultValue("Default string")
+        property: string;
+      }
+      class Child extends Parent {
+        constructor() {
+          super();
+        }
+      }
+      const instance = new Child();
+      expect(instance.property).toEqual("Default string");
+      instance.property = "New string";
+      expect(instance.property).toEqual("New string");
+    });
   });
 });
